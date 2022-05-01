3680f8bf403c7758493fb51b170399cd7c7061c9
sskim91 <tjdtjq91@gmail.com>
2022-05-01 23:09:50 +0900

    기본 문법

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,33 @@
+package study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sskim on 2022/05/01
+ * Github : http://github.com/sskim91
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
